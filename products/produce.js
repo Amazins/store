ffda56c6be964f4fd43e4dc16a8611a556451b40
TTS <90527370+TallerThanShort@users.noreply.github.com>
2022-10-04 20:01:08 +0200

    Update produce.js

--- products/produce.js
+++ products/produce.js
@@ -1,5 +1,6 @@
 const fragment = new URLSearchParams(window.location.search.slice(1));
 const [item, rand, player] = [fragment.get('product'), fragment.get('hash'), fragment.get('trkId')];
+document.title = `Products | Amazins`
 
 if(item, rand, player != null){
     console.log('ok')
