3ab76f5b4ed96b2c5a277b8631054ec006caaf64
TTS <90527370+TallerThanShort@users.noreply.github.com>
2022-10-04 20:05:57 +0200

    Fix /products bug

--- script.js
+++ script.js
@@ -51,7 +51,7 @@ const setupSite = (data) => {
     data.forEach(doc => {
         const item = doc.data();
         const li = `
-        <div onclick="location.href='https://amazins.github.io/store/products/?product=${item.sku}&hash=${hashStringer}&trkId=${trkString}';" title="Buy ${item.name}">
+        <div onclick="location.href='https://amazins.github.io/store/products?product=${item.sku}&hash=${hashStringer}&trkId=${trkString}';" title="Buy ${item.name}">
             <div class="slot">
                 <div class="banner">
                     <img class="banner-img" src="${item.image}">
