fa721c012dae48fd65ec22a68e7dc85b507d179c
TTS <90527370+TallerThanShort@users.noreply.github.com>
2022-12-03 19:26:58 +0100

    Shipping Prices Update
    List:
    - Shipping Prices are now based on product
    - Embed is smoother

--- products/produce.js
+++ products/produce.js
@@ -1,5 +1,5 @@
 const fragment = new URLSearchParams(window.location.search.slice(1));
-const [item, rand, player] = [fragment.get('product'), fragment.get('hash'), fragment.get('trkId')];
+const [item, rand, player, shiPrice] = [fragment.get('product'), fragment.get('hash'), fragment.get('trkId'), '23.75'];
 
 const firebaseConfig = {
     apiKey: "AIzaSyCImQ1forKpA4QqYDUBdgnHrWuRrDoe5JE",
@@ -20,33 +20,63 @@ if(item, rand, player != null){
         querySnapshot.forEach((doc) => {
             const pepo = doc.data();
             document.title = `${pepo.name} | Amazins`
-            document.querySelector(".app-container").innerHTML = 
-            `
-            <div style="color: white; position: relative;">
-                <h1 style="text-decoration: underline; cursor: default;">${pepo.name}</h1>
-                <ul class="images">
-                    <li>
-                        <img src="${pepo.image}">
-                    </li>
-                    <li>
-                        <img src="${pepo.img2}">
-                    </li>
-                    <li>
-                        <img src="${pepo.img3}">
-                    </li>
-                    <li>
-                        <img src="${pepo.img4}">
-                    </li>
-                </ul>
-            </div>
-            <div id="descriptionz" style="color: white;">
-                <p id="basic">${pepo.desc}<br>Color: ${pepo.color}<br>Price: $${pepo.price} + Shipping<br>Logo: ${pepo.merch}</p>
-                <button onclick="location.href='https://amazins.github.io/store/purchase?item=${pepo.sku}&page=1&trkId=${player}';" style="background-color: #F2E941; height: 39px; border: none; width: 98.6%; height: 24px; cursor: pointer;">Buy ${pepo.name}</button><br>
-                <p>Advanced Description:<br>${pepo.advanced}</p>
-                <p></p>
-            </div>
-            <div style="background-color:white;">${pepo.table}</div>
-            <button onclick="location.href='https://amazins.github.io/store/purchase?item=${pepo.sku}&page=1&trkId=${player}';" style="background-color: yellow; border: none; width: 100%; height: 32px; cursor: pointer;" title="Buy ${pepo.name}">Buy ${pepo.name}</button>`
+            if(pepo.ship != undefined){
+                document.querySelector(".app-container").innerHTML = 
+                `
+                <div style="color: white; position: relative;">
+                    <h1 style="text-decoration: underline; cursor: default;">${pepo.name}</h1>
+                    <ul class="images">
+                        <li>
+                            <img src="${pepo.image}">
+                        </li>
+                        <li>
+                            <img src="${pepo.img2}">
+                        </li>
+                        <li>
+                            <img src="${pepo.img3}">
+                        </li>
+                        <li>
+                            <img src="${pepo.img4}">
+                        </li>
+                    </ul>
+                </div>
+                <div id="descriptionz" style="color: white;">
+                    <p id="basic">${pepo.desc}<br>Color: ${pepo.color}<br>Price: $${pepo.price} ($${pepo.ship} Shipping)<br>Logo: ${pepo.merch}</p>
+                    <button onclick="location.href='https://amazins.github.io/store/purchase?item=${pepo.sku}&page=1&trkId=${player}';" style="background-color: #F2E941; height: 39px; border: none; width: 98.6%; height: 24px; cursor: pointer;">Buy ${pepo.name}</button><br>
+                    <p>Advanced Description:<br>${pepo.advanced}</p>
+                    <p></p>
+                </div>
+                <div style="background-color:white;">${pepo.table}</div>
+                <button onclick="location.href='https://amazins.github.io/store/purchase?item=${pepo.sku}&page=1&trkId=${player}';" style="background-color: yellow; border: none; width: 100%; height: 32px; cursor: pointer;" title="Buy ${pepo.name}">Buy ${pepo.name}</button>`
+            } else {
+                document.querySelector(".app-container").innerHTML = 
+                `
+                <div style="color: white; position: relative;">
+                    <h1 style="text-decoration: underline; cursor: default;">${pepo.name}</h1>
+                    <ul class="images">
+                        <li>
+                            <img src="${pepo.image}">
+                        </li>
+                        <li>
+                            <img src="${pepo.img2}">
+                        </li>
+                        <li>
+                            <img src="${pepo.img3}">
+                        </li>
+                        <li>
+                            <img src="${pepo.img4}">
+                        </li>
+                    </ul>
+                </div>
+                <div id="descriptionz" style="color: white;">
+                    <p id="basic">${pepo.desc}<br>Color: ${pepo.color}<br>Price: $${pepo.price} ($${shiPrice} Shipping)<br>Logo: ${pepo.merch}</p>
+                    <button onclick="location.href='https://amazins.github.io/store/purchase?item=${pepo.sku}&page=1&trkId=${player}';" style="background-color: #F2E941; height: 39px; border: none; width: 98.6%; height: 24px; cursor: pointer;">Buy ${pepo.name}</button><br>
+                    <p>Advanced Description:<br>${pepo.advanced}</p>
+                    <p></p>
+                </div>
+                <div style="background-color:white;">${pepo.table}</div>
+                <button onclick="location.href='https://amazins.github.io/store/purchase?item=${pepo.sku}&page=1&trkId=${player}';" style="background-color: yellow; border: none; width: 100%; height: 32px; cursor: pointer;" title="Buy ${pepo.name}">Buy ${pepo.name}</button>`
+            }
         })
     })
 } else{
--- purchase/borough.js
+++ purchase/borough.js
@@ -22,70 +22,134 @@ if(item != null){
         db.collection('warehouse').doc('items').collection('available').where("sku", "==", item).get().then((querySnapshot) => {
             querySnapshot.forEach((doc) => {
                 const buynow = doc.data();
-                titak = parseFloat(buynow.price) + parseFloat(shiPrice);
-                document.querySelector(".app-container").innerHTML = 
-                `
-                <div id="info">
-                    <h3 id="trycatch">Purchasing ${buynow.name} - $${buynow.price}</h3>
-                    <p>Color:</p>
-                </div>
-                <div id="flow" style="position: relative;">
-                    <input style="width: 100%; height: 20px;" id="id" placeholder="${buynow.color}" disabled="disabled"><br>
-                    <label for="select-size">OPTIONAL - Choose a size:</label>
-                    <select name="select-size" id="select-size">
-                        <option value="S">S</option>
-                        <option value="M">M</option>
-                        <option value="L">L</option>
-                        <option value="XL">XL</option>
-                        <option value="2XL">2XL</option>
-                        <option value="3XL">3XL</option>
-                    </select>
-                    <br>
-                    <h3>Shipping Info</h3>
-                    <p>Recipient</p>
-                    <input style="width: 45%; height: 20px;" id="buyer_name" placeholder="First Name" maxlength="100" required=""><br>
-                    <input style="position: absolute; top: 143px; left: 50%; width: 45%; height: 20px;" id="second_name" placeholder="Last Name" maxlength="100" required=""><br>
-                    <input style="width: 100%; height: 20px;" type="number" id="phone_number" placeholder="Phone Number (enter country code)" maxlength="16" required=""><br>
-                    <input style="width: 100%; height: 20px;" type="email" id="email" placeholder="Email (optional)"><br>
-                    <input style="width: 45%; height: 20px;" type="text" id="country_name" placeholder="Country" maxlength="20" required=""><br>
-                    <input style="position: absolute; top: 240px; left: 50%; width: 45%; height: 20px;" type="text" id="prov_name" placeholder="State, Province, or Region">
-                    <input style="width: 100%; height: 20px;" type="text" id="city" placeholder="Town or City" required=""><br>
-                    <input style="width: 100%; height: 20px;" type="text" id="address" placeholder="Address Line 1" required=""><br>
-                    <input style="width: 100%; height: 20px;" type="text" id="address_two" placeholder="Address Line 2 (Optional)"><br>
-                    <input style="width: 100%; height: 20px;" type="number" id="post_code" placeholder="Postal Code" maxlength="20" required=""><br>
-                    <p>Standard Shipping: $${shiPrice} (Including $5.05 tax)</p><br>
-                    <p>Total: $${titak}</p>
-                    <button style="background-color: green; color: white; width: 100%; height: 40px; font-size: 20px; cursor: pointer;" onclick="addOrder()">Place Order</button>
-                    <p style="color: crimson;">By continuing, you agree to allow Amazins PLC to process any data submitted through this form</p>
-                </div>
-                `
+                if(buynow.ship != undefined){
+                    titak = parseFloat(buynow.price) + parseFloat(buynow.ship);
+                    document.querySelector(".app-container").innerHTML = 
+                    `
+                    <div id="info">
+                        <h3 id="trycatch">Purchasing ${buynow.name} - $${buynow.price}</h3>
+                        <p>Color:</p>
+                    </div>
+                    <div id="flow" style="position: relative;">
+                        <input style="width: 100%; height: 20px;" id="id" placeholder="${buynow.color}" disabled="disabled"><br>
+                        <label for="select-size">OPTIONAL - Choose a size:</label>
+                        <select name="select-size" id="select-size">
+                            <option value="S">S</option>
+                            <option value="M">M</option>
+                            <option value="L">L</option>
+                            <option value="XL">XL</option>
+                            <option value="2XL">2XL</option>
+                            <option value="3XL">3XL</option>
+                        </select>
+                        <br>
+                        <h3>Shipping Info</h3>
+                        <p>Recipient</p>
+                        <input style="width: 45%; height: 20px;" id="buyer_name" placeholder="First Name" maxlength="100" required=""><br>
+                        <input style="position: absolute; top: 143px; left: 50%; width: 45%; height: 20px;" id="second_name" placeholder="Last Name" maxlength="100" required=""><br>
+                        <input style="width: 100%; height: 20px;" type="number" id="phone_number" placeholder="Phone Number (enter country code)" maxlength="16" required=""><br>
+                        <input style="width: 100%; height: 20px;" type="email" id="email" placeholder="Email (optional)"><br>
+                        <input style="width: 45%; height: 20px;" type="text" id="country_name" placeholder="Country" maxlength="20" required=""><br>
+                        <input style="position: absolute; top: 240px; left: 50%; width: 45%; height: 20px;" type="text" id="prov_name" placeholder="State, Province, or Region">
+                        <input style="width: 100%; height: 20px;" type="text" id="city" placeholder="Town or City" required=""><br>
+                        <input style="width: 100%; height: 20px;" type="text" id="address" placeholder="Address Line 1" required=""><br>
+                        <input style="width: 100%; height: 20px;" type="text" id="address_two" placeholder="Address Line 2 (Optional)"><br>
+                        <input style="width: 100%; height: 20px;" type="number" id="post_code" placeholder="Postal Code" maxlength="20" required=""><br>
+                        <p>Standard Shipping: $${buynow.ship} (Including $5.05 tax)</p><br>
+                        <p>Total: $${titak}</p>
+                        <button style="background-color: green; color: white; width: 100%; height: 40px; font-size: 20px; cursor: pointer;" onclick="addOrder()">Place Order</button>
+                        <p style="color: crimson;">By continuing, you agree to allow Amazins PLC to process any data submitted through this form.<br>Email is used to send shipping notifications, phone number is used for calling the residence if no one is present.<br><b>Orders to the EU, UK and Norway - VAT is applied.</b></p>
+                    </div>
+                    `
+                } else {
+                    titak = parseFloat(buynow.price) + parseFloat(shiPrice);
+                    document.querySelector(".app-container").innerHTML = 
+                    `
+                    <div id="info">
+                        <h3 id="trycatch">Purchasing ${buynow.name} - $${buynow.price}</h3>
+                        <p>Color:</p>
+                    </div>
+                    <div id="flow" style="position: relative;">
+                        <input style="width: 100%; height: 20px;" id="id" placeholder="${buynow.color}" disabled="disabled"><br>
+                        <label for="select-size">OPTIONAL - Choose a size:</label>
+                        <select name="select-size" id="select-size">
+                            <option value="S">S</option>
+                            <option value="M">M</option>
+                            <option value="L">L</option>
+                            <option value="XL">XL</option>
+                            <option value="2XL">2XL</option>
+                            <option value="3XL">3XL</option>
+                        </select>
+                        <br>
+                        <h3>Shipping Info</h3>
+                        <p>Recipient</p>
+                        <input style="width: 45%; height: 20px;" id="buyer_name" placeholder="First Name" maxlength="100" required=""><br>
+                        <input style="position: absolute; top: 143px; left: 50%; width: 45%; height: 20px;" id="second_name" placeholder="Last Name" maxlength="100" required=""><br>
+                        <input style="width: 100%; height: 20px;" type="number" id="phone_number" placeholder="Phone Number (enter country code)" maxlength="16" required=""><br>
+                        <input style="width: 100%; height: 20px;" type="email" id="email" placeholder="Email (optional)"><br>
+                        <input style="width: 45%; height: 20px;" type="text" id="country_name" placeholder="Country" maxlength="20" required=""><br>
+                        <input style="position: absolute; top: 240px; left: 50%; width: 45%; height: 20px;" type="text" id="prov_name" placeholder="State, Province, or Region">
+                        <input style="width: 100%; height: 20px;" type="text" id="city" placeholder="Town or City" required=""><br>
+                        <input style="width: 100%; height: 20px;" type="text" id="address" placeholder="Address Line 1" required=""><br>
+                        <input style="width: 100%; height: 20px;" type="text" id="address_two" placeholder="Address Line 2 (Optional)"><br>
+                        <input style="width: 100%; height: 20px;" type="number" id="post_code" placeholder="Postal Code" maxlength="20" required=""><br>
+                        <p>Standard Shipping: $${shiPrice} (Including $5.05 tax)</p><br>
+                        <p>Total: $${titak}</p>
+                        <button style="background-color: yellow; color: black; width: 100%; height: 40px; font-size: 20px; cursor: pointer;" onclick="addOrder()">Place Order</button>
+                        <p style="color: crimson;">By continuing, you agree to allow Amazins PLC to process any data submitted through this form.<br>Email is used to send shipping notifications, phone number is used for calling the residence if no one is present.<br><b>Orders to the EU, UK and Norway - VAT is applied.</b></p>
+                    </div>
+                    `
+                }
             })
         })
     } else if(page === '2'){
         db.collection('warehouse').doc('items').collection('available').where("sku", "==", item).get().then((querySnapshot) => {
             querySnapshot.forEach((doc) => {
                 const buynow = doc.data();
-                titak = parseFloat(buynow.price) + parseFloat(shiPrice);
-                document.querySelector(".app-container").innerHTML = 
-                `
-                <div id="info">
-                    <h3 id="trycatch">Purchasing ${buynow.name} - $${buynow.price}</h3>
-                </div>
-                <div id="flow" style="position: relative;">
-                    
-                    <h3>Card Info</h3>
-                    <p>Recipient</p>
-                    <input style="width: 45%; height: 20px;" id="card_fname" placeholder="First Name as on Card" required=""><br>
-                    <input style="position: absolute; top: 78px; left: 50%; width: 45%; height: 20px;" id="card_sname" placeholder="Last Name as on Card" maxlength="100" required=""><br>
-                    <input style="width: 95%; height: 20px;" type="text" id="card_fo" placeholder="Card Number" maxlength="16" required=""><br>
-                    <input style="width: 45%; height: 20px;" type="text" id="csv" placeholder="Security Code on backside (CSV)" maxlength="6" required=""><br>
-                    <input style="position: absolute; top: 149px; left: 50%; width: 45%; height: 20px;" type="month" id="expiry" placeholder="Card Expiry" required=""><br>
-                    <p>Standard Shipping: $${shiPrice} (Including $5.05 VAT)</p><br>
-                    <p>Total: $${titak}</p>
-                    <button style="background-color: green; color: white; width: 100%; height: 40px; font-size: 20px; cursor: pointer;" onclick="fulFill()">Fulfill Order</button>
-                    <b style="color: crimson;">By continuing, you are confirming that you are either the cardholder of mentioned card, or authorised to use it for this order. Amazins PLC takes no charge of theft or missuse across the platform. Payments may take up to 24 hours to go through.</b>
-                </div>
-                `
+                if(buynow.ship != undefined){
+                    titak = parseFloat(buynow.price) + parseFloat(buynow.ship);
+                    document.querySelector(".app-container").innerHTML = 
+                    `
+                    <div id="info">
+                        <h3 id="trycatch">Purchasing ${buynow.name} - $${buynow.price}</h3>
+                    </div>
+                    <div id="flow" style="position: relative;">
+                        
+                        <h3>Card Info</h3>
+                        <p>Recipient</p>
+                        <input style="width: 45%; height: 20px;" id="card_fname" placeholder="First Name as on Card" required=""><br>
+                        <input style="position: absolute; top: 78px; left: 50%; width: 45%; height: 20px;" id="card_sname" placeholder="Last Name as on Card" maxlength="100" required=""><br>
+                        <input style="width: 95%; height: 20px;" type="text" id="card_fo" placeholder="Card Number" maxlength="16" required=""><br>
+                        <input style="width: 45%; height: 20px;" type="text" id="csv" placeholder="Security Code on backside (CSV)" maxlength="6" required=""><br>
+                        <input style="position: absolute; top: 149px; left: 50%; width: 45%; height: 20px;" type="month" id="expiry" placeholder="Card Expiry" required=""><br>
+                        <p>Standard Shipping: $${buynow.ship} (Including $5.05 VAT)</p><br>
+                        <p>Total: $${titak}</p>
+                        <button style="background-color: green; color: white; width: 100%; height: 40px; font-size: 20px; cursor: pointer;" onclick="fulFill()">Fulfill Order</button>
+                        <b style="color: crimson;">By continuing, you are confirming that you are either the cardholder of mentioned card, or authorised to use it for this order. Amazins PLC takes no charge of theft or missuse across the platform. Payments may take up to 24 hours to go through.</b>
+                    </div>
+                    `
+                } else {
+                    titak = parseFloat(buynow.price) + parseFloat(shiPrice);
+                    document.querySelector(".app-container").innerHTML = 
+                    `
+                    <div id="info">
+                        <h3 id="trycatch">Purchasing ${buynow.name} - $${buynow.price}</h3>
+                    </div>
+                    <div id="flow" style="position: relative;">
+                        
+                        <h3>Card Info</h3>
+                        <p>Recipient</p>
+                        <input style="width: 45%; height: 20px;" id="card_fname" placeholder="First Name as on Card" required=""><br>
+                        <input style="position: absolute; top: 78px; left: 50%; width: 45%; height: 20px;" id="card_sname" placeholder="Last Name as on Card" maxlength="100" required=""><br>
+                        <input style="width: 95%; height: 20px;" type="text" id="card_fo" placeholder="Card Number" maxlength="16" required=""><br>
+                        <input style="width: 45%; height: 20px;" type="text" id="csv" placeholder="Security Code on backside (CSV)" maxlength="6" required=""><br>
+                        <input style="position: absolute; top: 149px; left: 50%; width: 45%; height: 20px;" type="month" id="expiry" placeholder="Card Expiry" required=""><br>
+                        <p>Standard Shipping: $${shiPrice} (Including $5.05 VAT)</p><br>
+                        <p>Total: $${titak}</p>
+                        <button style="background-color: green; color: white; width: 100%; height: 40px; font-size: 20px; cursor: pointer;" onclick="fulFill()">Fulfill Order</button>
+                        <b style="color: crimson;">By continuing, you are confirming that you are either the cardholder of mentioned card, or authorised to use it for this order. Amazins PLC takes no charge of theft or missuse across the platform. Payments may take up to 48 hours to go through.</b>
+                    </div>
+                    `
+                }
             })
         })
     } else {
